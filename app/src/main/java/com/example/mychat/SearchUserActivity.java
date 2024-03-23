26eff86ec824bec69bfd477f88b27f8e6a125f83
Guillermo Chamale <gchamalef@miumg.edu.gt>
2024-03-22 18:48:39 -0600

    Disenio de pagina para buscar usuario

--- app/src/main/java/com/example/mychat/SearchUserActivity.java
+++ app/src/main/java/com/example/mychat/SearchUserActivity.java
@@ -1,18 +1,52 @@
 package com.example.mychat;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class SearchUserActivity extends AppCompatActivity {
+
+    EditText searchInput;
+    ImageButton searchButton;
+    ImageButton backButton;
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_user);
+
+        searchInput = findViewById(R.id.search_username_input);
+        searchButton = findViewById(R.id.search_user_btn);
+        backButton = findViewById(R.id.back_btn);
+        recyclerView = findViewById(R.id.search_user_recycler_view);
+
+        searchInput.requestFocus();
+
+        backButton.setOnClickListener(v -> {
+            onBackPressed();
+        });
+
+        searchButton.setOnClickListener(v -> {
+            String searchTerm = searchInput.getText().toString();
+            if(searchTerm.isEmpty() || searchTerm.length()<3){
+                searchInput.setError("Nombre de usuario invalido.");
+                return;
+            }
+            setupSearchReciclerView(searchTerm);
+        });
+
     }
+
+    void setupSearchReciclerView(String searchTerm){
+
+    }
+
 }
